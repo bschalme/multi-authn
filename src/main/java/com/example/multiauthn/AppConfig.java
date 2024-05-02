d5db2f6a5441b1e1fb2631572f226cf1ff0d837a
Brian Schalme <bschalme@airspeed.ca>
2024-05-02 10:10:13 -0500

    Removed a redundant bean definition.

--- src/main/java/com/example/multiauthn/AppConfig.java
+++ src/main/java/com/example/multiauthn/AppConfig.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
@@ -27,14 +23,5 @@ public class AppConfig {
 	public RequestContextListener requestContextListener() {
 		return new RequestContextListener();
 	}
-    @Bean
-    public ClientRegistrationRepository clientRepository() {
-        ClientRegistration githubRegistration = CommonOAuth2Provider.GITHUB.getBuilder("github")
-                .clientId(githubClientId)
-                .clientSecret(githubClientSecret)
-                .build();
-
-        return new InMemoryClientRegistrationRepository(githubRegistration);
-    }
 
 }
