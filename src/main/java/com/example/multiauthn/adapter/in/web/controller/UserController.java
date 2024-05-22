ca50e78c196d383d895f475f9334c45cd398af3b
Brian Schalme <bschalme@airspeed.ca>
2024-05-22 13:10:40 -0500

    Demonstrate that the access and refresh tokens are available after logging in.

--- src/main/java/com/example/multiauthn/adapter/in/web/controller/UserController.java
+++ src/main/java/com/example/multiauthn/adapter/in/web/controller/UserController.java
@@ -1,13 +1,32 @@
 package com.example.multiauthn.adapter.in.web.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
 @Controller
 public class UserController {
 
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
     @GetMapping("/user")
-    public String userHome() {
+    public String userHome(Authentication authn) {
+        OAuth2AuthorizedClient authorizedClient = this.authorizedClientService.loadAuthorizedClient("keycloak",
+                authn.getName());
+
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+        OAuth2RefreshToken refreshToken = authorizedClient.getRefreshToken();
+        log.debug("Access token = {}", accessToken.getTokenValue());
+        log.debug("Refresh token = {}", refreshToken != null ? refreshToken.getTokenValue() : "(null)");
         return "user";
     }
 }
